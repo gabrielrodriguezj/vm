9aed72abb259159fca1388f9f8b371d2e7d65826
Gabriel Rodríguez <gabrielrodriguezj@hotmail.com>
2025-12-30 20:55:33 -0600

    First commit

---
+++ common.h
@@ -0,0 +1,10 @@
+#ifndef VM_COMMON_H
+#define VM_COMMON_H
+
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
+
+
+
+#endif //VM_COMMON_H
---
+++ debug.h
@@ -0,0 +1,9 @@
+#ifndef VM_DEBUG_H
+#define VM_DEBUG_H
+
+#include "chunk.h"
+
+void disassembleChunk(Chunk* chunk, const char* name);
+int disassembleInstruction(Chunk* chunk, int offset);
+
+#endif //VM_DEBUG_H
