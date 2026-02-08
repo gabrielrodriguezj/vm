9dde5e2992cfe32ebbf5b0442e94722879a4f07d
Gabriel Rodríguez <gabrielrodriguezj@hotmail.com>
2026-02-08 00:06:35 -0600

    Type punning
    Creating structures for handling strings, instances of classes. Obj structure.

---
+++ object.h
@@ -0,0 +1,53 @@
+#ifndef VM_OBJECT_H
+#define VM_OBJECT_H
+
+/**
+ * Interesting definition: type punning
+ *
+ * It’s an example of type punning, but that term is too broad. In the absence
+ * of any better ideas, I’ll call it struct inheritance, because it relies
+ * on structs and roughly follows how single-inheritance of state works in
+ * object-oriented languages.
+ *
+ * Like a tagged union, each Obj starts with a tag field that identifies
+ * what kind of object it is—string, instance, etc. Following that are the
+ * payload fields. Instead of a union with cases for each type, each type
+ * is its own separate struct.  The tricky part is how to treat these structs
+ * uniformly since C  * has no concept of inheritance or polymorphism.
+ * I’ll explain that soon,  * but first lets get the preliminary stuff out
+ * of the way.
+ *
+ * "Robert Nystrom"
+ */
+
+
+#include "common.h"
+#include "value.h"
+
+#define OBJ_TYPE(value)        (AS_OBJ(value)->type)
+
+#define IS_STRING(value)       isObjType(value, OBJ_STRING)
+
+#define AS_STRING(value)       ((ObjString*)AS_OBJ(value))
+#define AS_CSTRING(value)      (((ObjString*)AS_OBJ(value))->chars)
+
+typedef enum {
+    OBJ_STRING,
+} ObjType;
+
+struct Obj {
+    ObjType type;
+};
+
+struct ObjString {
+    Obj obj;
+    int length;
+    char* chars;
+};
+
+static inline bool isObjType(Value value, ObjType type) {
+    return IS_OBJ(value) && AS_OBJ(value)->type == type;
+}
+
+
+#endif //VM_OBJECT_H
--- value.h
+++ value.h
@@ -3,10 +3,16 @@
 
 #include "common.h"
 
+typedef struct Obj Obj;
+typedef struct ObjString ObjString;
+
 typedef enum {
     VAL_BOOL,
     VAL_NULL,
     VAL_NUMBER,
+    VAL_OBJ,
+    // When a Value’s type is VAL_OBJ, the payload is a pointer to the
+    // heap memory, so we add another case to the union for that.
   } ValueType;
 
 // Data structure "Tagged union"
@@ -15,6 +21,7 @@ typedef struct {
     union {
         bool boolean;
         double number;
+        Obj* obj;
     } as;
 } Value;
 
@@ -22,15 +29,18 @@ typedef struct {
 #define IS_BOOL(value)    ((value).type == VAL_BOOL)
 #define IS_NULL(value)     ((value).type == VAL_NULL)
 #define IS_NUMBER(value)  ((value).type == VAL_NUMBER)
+#define IS_OBJ(value)     ((value).type == VAL_OBJ)
 
 // There’s no AS_NULL macro because there is only one null value, so
-// a Value with type VAL_NULL doesn’t carry any extra data.
+// a Value with type VAL_NULL doesn't carry any extra data.
+#define AS_OBJ(value)     ((value).as.obj)
 #define AS_BOOL(value)    ((value).as.boolean)
 #define AS_NUMBER(value)  ((value).as.number)
 
 #define BOOL_VAL(value)   ((Value){VAL_BOOL, {.boolean = value}})
 #define NULL_VAL           ((Value){VAL_NULL, {.number = 0}})
 #define NUMBER_VAL(value) ((Value){VAL_NUMBER, {.number = value}})
+#define OBJ_VAL(object)   ((Value){VAL_OBJ, {.obj = (Obj*)object}})
 
 typedef struct {
     int capacity;
